ba0c702831686cac0f2eb549a98546812e5d59dc
Phu Nguyen <phu.nguyen@aller.com>
2021-03-26 14:08:31 +0200

    test action again

--- src/components/InboxScreen.js
+++ src/components/InboxScreen.js
@@ -11,7 +11,7 @@ export function PureInboxScreen({ error }) {
       <div className="page lists-show">
         <div className="wrapper-message">
           <span className="icon-face-sad" />
-          <div className="title-message">Oh no!</div>
+          <div className="title-message">Oh no, oh no, oh no no no no no!</div>
           <div className="subtitle-message">Something went wrong</div>
         </div>
       </div>
@@ -21,7 +21,7 @@ export function PureInboxScreen({ error }) {
     <div className="page lists-show">
       <nav>
         <h1 className="title-page">
-          <span className="title-wrapper">Taskbox</span>
+          <span className="title-wrapper">Taskbox list</span>
         </h1>
       </nav>
       <TaskList />
